37a3128b8124121231196354fb2476c5255d4776
Joshua <c.treyce@gmail.com>
2014-05-13 21:47:32 -0600

    Cleaned up rotate.py and fixed 'off' profile detection

--- rotate.py
+++ rotate.py
@@ -1,34 +1,48 @@
+# pylint: disable=C0103,C0301
 
-#Modules General
-import os,sys,lightpack
+# Modules General
+import os, lightpack
 from sys import argv
 
 # Modules XBMC
 import xbmc, xbmcgui, xbmcaddon
 
 __settings__ = xbmcaddon.Addon("script.lightpack")
+__language__ = __settings__.getLocalizedString
 
-lpack = lightpack.lightpack(__settings__.getSetting("host"), int(__settings__.getSetting("port")), __settings__.getSetting("apikey"), [1,2,3,4,5,6,7,8,9,10] )
+lphost = __settings__.getSetting("host")
+lpport = int(__settings__.getSetting("port"))
+lpapikey = __settings__.getSetting("apikey")
+
+lpack = lightpack.lightpack(lphost, lpport, lpapikey, range(1, 11))
 lpack.connect()
+
 profiles = lpack.getProfiles()
 del profiles[-1]
-# profiles = ["profile1", "profile2", "profile3"]
-profiles.append("off")
+profiles.append(__language__(32071))
+off = len(profiles)-1
 
 last_profile = __settings__.getSetting("last_profile")
-if (last_profile == ""):
-  last_profile_index = -1
+if last_profile == "":
+    last_profile_index = -1
 else:
-  last_profile_index = profiles.index(last_profile)
+    last_profile_index = profiles.index(last_profile)
 next_profile_index = last_profile_index + 1
-if (next_profile_index > len(profiles)-1):
-  next_profile_index = 0
+if next_profile_index > len(profiles)-1:
+    next_profile_index = 0
 
 lpack.lock()
 lpack.turnOn()
-next = profiles[next_profile_index]
-lpack.setProfile(profiles[next_profile_index])
-lpack.unlock
+
+if off == int(next_profile_index):
+    lpack.turnOff()
+else:
+    lpack.turnOn()
+    lpack.setProfile(profiles[next_profile_index])
+
+lpack.unlock()
 lpack.disconnect()
-__settings__.setSetting("last_profile",profiles[next_profile_index])
+
+# Store profile name for display and notify user of profile change
+__settings__.setSetting("last_profile", profiles[next_profile_index])
 xbmc.executebuiltin('Notification(%s,%s)'%("LightPack", profiles[next_profile_index]))
