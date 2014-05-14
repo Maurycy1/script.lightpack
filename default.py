99685e2fd0ba32d2ff5608af1f49b735c42c568d
Joshua <c.treyce@gmail.com>
2014-05-13 21:42:04 -0600

    Cleanup of default.py

--- default.py
+++ default.py
@@ -1,32 +1,37 @@
+# pylint: disable=C0103,C0301
 
-#Modules General
-import os,lightpack
+# Modules General
+import os, lightpack
 from sys import argv
 
 # Modules XBMC
 import xbmc, xbmcgui, xbmcaddon
 
-__settings__ = xbmcaddon.Addon( "script.lightpack" )
+__settings__ = xbmcaddon.Addon("script.lightpack")
 __language__ = __settings__.getLocalizedString
-#########################################################################################################
-## BEGIN
-#########################################################################################################
-lpack = lightpack.lightpack(__settings__.getSetting("host"), int(__settings__.getSetting("port")), __settings__.getSetting("apikey"), [1,2,3,4,5,6,7,8,9,10] )
+
+lphost = __settings__.getSetting("host")
+lpport = int(__settings__.getSetting("port"))
+lpapikey = __settings__.getSetting("apikey")
+
+lpack = lightpack.lightpack(lphost, lpport, lpapikey, range(1, 11))
 lpack.connect()
+
 menu = lpack.getProfiles()
 del menu[-1]
 menu.append(__language__(32071))
 off = len(menu)-1
-quit = False
-while not quit:
-	selected = xbmcgui.Dialog().select(__language__(32070), menu )
-	if selected!= -1:
-		lpack.lock()
-		if (off==int(selected)):
-			lpack.turnOff()
-		else:
-			lpack.turnOn()
-			lpack.setProfile(menu[selected])
-		lpack.unlock
-	quit = True
+
+lpquit = False
+while not lpquit:
+    selected = xbmcgui.Dialog().select(__language__(32070), menu)
+    if selected != -1:
+        lpack.lock()
+        if off == int(selected):
+            lpack.turnOff()
+        else:
+            lpack.turnOn()
+            lpack.setProfile(menu[selected])
+        lpack.unlock()
+    lpquit = True
 lpack.disconnect()
